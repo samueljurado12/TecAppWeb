ff2d5bedb344b8300858bd3e338aa26c634268d7
Javaamo <javiervazmor@gmail.com>
2018-05-04 17:10:30 +0200

    employee just employee
    Employee just employee.
    Normal user all pages.

--- 4Bank_APP/4Bank_APP-war/src/java/servlets/LoginServlet.java
+++ 4Bank_APP/4Bank_APP-war/src/java/servlets/LoginServlet.java
@@ -46,6 +46,11 @@ public class LoginServlet extends HttpServlet {
             session.setAttribute("user", user);
             session.setAttribute("selectedAccount", null);
             RequestDispatcher rd = request.getRequestDispatcher("ListMovement");
+            if(user.getEmployee()!=null){
+                if(user.getEmployee().getIsEmployee()){
+                    rd =request.getRequestDispatcher("employee");
+                }
+            }
             if (user.getEmployee() != null) {
                 session.setAttribute("isEmployee", user.getEmployee().getIsEmployee());
             } else {

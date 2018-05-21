437e2264f629462fb4ee6644c2e51843654a2333
samueljurado12 <samueljurado12@gmail.com>
2018-05-21 11:31:45 +0200

    Remove useless code

--- 4Bank_JSF/4Bank_JSF-war/src/java/managedBeans/SessionUtils.java
+++ 4Bank_JSF/4Bank_JSF-war/src/java/managedBeans/SessionUtils.java
@@ -7,9 +7,7 @@ package managedBeans;
  */
 
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import persistence.User;
 
 /**
  *
@@ -24,31 +22,4 @@ public class SessionUtils {
 				.getExternalContext().getSession(false);
 	}
 
-	public static HttpServletRequest getRequest() {
-		return (HttpServletRequest) FacesContext.getCurrentInstance()
-				.getExternalContext().getRequest();
-	}
-
-	public static String getUserName() {
-		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
-				.getExternalContext().getSession(false);
-		return session.getAttribute("username").toString();
-	}
-
-	public static String getUserId() {
-		HttpSession session = getSession();
-		if (session != null)
-			return (String) session.getAttribute("userid");
-		else
-			return null;
-	}
-        
-        public static User getActiveUser(){
-            HttpSession session = getSession();
-            if(session != null){
-                return (User) session.getAttribute("user");
-            } else {
-                return null;
-            }
-        }
 }
